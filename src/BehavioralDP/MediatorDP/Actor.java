ef3f635effe6ad99b4d21e465a1fd22acc5fab87
Fatih Deniz <deniz.fatih@hotmail.de>
2023-05-09 21:35:56 +0200

    Design Pattern - Day 3

---
+++ src/BehavioralDP/MediatorDP/Actor.java
@@ -0,0 +1,9 @@
+package BehavioralDP.MediatorDP;
+
+public interface Actor {
+
+    void receiveMessage(String message);
+
+    void sendMessage(String topic, String message);
+
+}
---
+++ src/BehavioralDP/MediatorDP/Dispatcher.java
@@ -0,0 +1,7 @@
+package BehavioralDP.MediatorDP;
+
+public interface Dispatcher {
+
+    void dispatch(String topic, String message);
+
+}
